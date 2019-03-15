7ab3b6cda68e3914d92149d566a25e12e8794cbd
GBTC408004UR <GBTC408004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-15 15:46:51 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+
+@Entity
+public class Message {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long id;
+
+        @NotNull
+        @Size(min = 4)
+        private String content;
+
+        @NotNull
+        @Size(min = 3)
+        private Date postddate;
+
+        @NotNull
+        @Size(min = 10)
+        private String sentby;
+
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public Date getPostddate() {
+            return postddate;
+        }
+
+        public void setPostddate(Date postddate) {
+            this.postddate = postddate;
+        }
+
+        public String getSentby() {
+            return sentby;
+        }
+
+        public void setSentby(String sentby) {
+            this.sentby = sentby;
+        }
+}
---
+++ src/main/java/com/example/demo/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+
+
+
+    import org.springframework.data.repository.CrudRepository;
+
+    public interface MessageRepository extends CrudRepository<Message,
+            Long> {
+
+    }
+
